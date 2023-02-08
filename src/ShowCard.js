6c6dd2e319042fe2b55dd0c3db662a3d258b258c
Gav1012 <gavin.poley@gmail.com>
2023-02-08 14:44:07 -0800

    made some progress, does not work

--- src/ShowCard.js
+++ src/ShowCard.js
@@ -1,43 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Typography from '@mui/material/Typography';
 // import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import { CardActionArea, CardContent, CardMedia } from "@mui/material";
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import {CardActionArea} from '@mui/material';
-
-// fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=10&limit_suggestions=1', {
-//       "method": "GET",
-//       "headers": {
-//         'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
-//         'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
-//       }
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       // holds a list of stuff pulled from API
-//       const list = data.titles;
-//       list.map((item) => {
-//         // only continue if data is a show
-//         if (item.summary.type == 'show') {
-//           console.log("this is a show");
-//           console.log(item);
-//           const name = item.jawSummary.title;
-//           const poster = item.jawSummary.backgroundImage.url;
-//           const show = `<li><img src="${poster}"> <h2>${name}</h2></li>`
-//           document.querySelector('.movies').innerHTML += show;
-
-//         }
-//       })
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     })
 
 function ShowCard() {
+    const [list, setList] = useState([]);
+    fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=2&limit_suggestions=1', {
+      "method": "GET",
+      "headers": {
+        'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
+        'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
+      }
+    })
+    .then(response => response.json())
+    .then((data) => setList(data))
+    // .then(data => {
+    //   // holds a list of stuff pulled from API
+    //   const list = data.titles;
+    //   list.map((item) => {
+    //     // only continue if data is a show
+    //     if (item.summary.type == 'show') {
+    //       console.log("this is a show");
+    //       console.log(item);
+    //       const name = item.jawSummary.title;
+    //       const poster = item.jawSummary.backgroundImage.url;
+    //       const show = `<li><img src="${poster}"> <h2>${name}</h2></li>`
+    //       document.querySelector('.movies').innerHTML += show;
+
+    //     }
+    //   })
+    // })
+    .catch(err => {
+      console.log(err);
+    });
+
+    // useEffect(() => {
+    //     fetchData();
+    // }, [])
+
     return(
-        <Card sx={{maxWidth: 345}}>
+        list.map((item) => {
+            <Card sx={{maxWidth: 345}}>
             <CardActionArea>
                 <CardMedia
                     component="img"
@@ -46,11 +50,12 @@ function ShowCard() {
                 />
                 <CardContent>
                     <Typography gutterBottom variant="subtitle1">
-                        Name of Show
+                        {item.jawSummary.title}
                     </Typography>
                 </CardContent>
             </CardActionArea>
         </Card>
+        })
     )
 }
 
