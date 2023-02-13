00473069b906e4ecd4dabfa5da808c2dc9490b0b
Gav1012 <gavin.poley@gmail.com>
2023-02-12 16:28:35 -0800

    commented out big chunks

--- src/ShowCard.js
+++ src/ShowCard.js
@@ -6,42 +6,35 @@ import { CardActionArea, CardContent, CardMedia } from "@mui/material";
 
 function ShowCard() {
     const [list, setList] = useState([]);
-    fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=2&limit_suggestions=1', {
-      "method": "GET",
-      "headers": {
-        'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
-        'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
-      }
-    })
-    .then(response => response.json())
-    .then((data) => setList(data))
-    // .then(data => {
-    //   // holds a list of stuff pulled from API
-    //   const list = data.titles;
-    //   list.map((item) => {
-    //     // only continue if data is a show
-    //     if (item.summary.type == 'show') {
-    //       console.log("this is a show");
-    //       console.log(item);
-    //       const name = item.jawSummary.title;
-    //       const poster = item.jawSummary.backgroundImage.url;
-    //       const show = `<li><img src="${poster}"> <h2>${name}</h2></li>`
-    //       document.querySelector('.movies').innerHTML += show;
+    // fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=2&limit_suggestions=1', {
+//       "method": "GET",
+//       "headers": {
+//         'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
+//         'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       // holds a list of stuff pulled from API
+//       const list = data.titles;
+//       list.map((item) => {
+//         // only continue if data is a show
+//         if (item.summary.type == 'show') {
+//           console.log("this is a show");
+//           console.log(item);
+//           const name = item.jawSummary.title;
+//           const poster = item.jawSummary.backgroundImage.url;
+//           const show = `<li><img src="${poster}"> <h2>${name}</h2></li>`
+//           document.querySelector('.movies').innerHTML += show;
 
-    //     }
-    //   })
-    // })
-    .catch(err => {
-      console.log(err);
-    });
-
-    // useEffect(() => {
-    //     fetchData();
-    // }, [])
-
+//         }
+//       })
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
     return(
-        list.map((item) => {
-            <Card sx={{maxWidth: 345}}>
+        <Card sx={{maxWidth: 345}}>
             <CardActionArea>
                 <CardMedia
                     component="img"
@@ -50,12 +43,11 @@ function ShowCard() {
                 />
                 <CardContent>
                     <Typography gutterBottom variant="subtitle1">
-                        {item.jawSummary.title}
+                        Name of Show
                     </Typography>
                 </CardContent>
             </CardActionArea>
         </Card>
-        })
     )
 }
 
