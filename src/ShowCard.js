1069416f16aaacf368b33d25423a47dd5f3ade29
Gav1012 <gavin.poley@gmail.com>
2023-02-12 20:06:58 -0800

    can display shows like Netflix, working on scrolling and click linking

--- src/ShowCard.js
+++ src/ShowCard.js
@@ -1,54 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Typography from '@mui/material/Typography';
-// import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import { CardActionArea, CardContent, CardMedia } from "@mui/material";
+import "./ShowCard.css";
 
 function ShowCard() {
     const [list, setList] = useState([]);
-    // fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=2&limit_suggestions=1', {
-//       "method": "GET",
-//       "headers": {
-//         'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
-//         'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
-//       }
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       // holds a list of stuff pulled from API
-//       const list = data.titles;
-//       list.map((item) => {
-//         // only continue if data is a show
-//         if (item.summary.type == 'show') {
-//           console.log("this is a show");
-//           console.log(item);
-//           const name = item.jawSummary.title;
-//           const poster = item.jawSummary.backgroundImage.url;
-//           const show = `<li><img src="${poster}"> <h2>${name}</h2></li>`
-//           document.querySelector('.movies').innerHTML += show;
-
-//         }
-//       })
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     })
+    fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=4&limit_suggestions=1', {
+      "method": "GET",
+      "headers": {
+        'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
+        'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
+      }
+    })
+    .then(response => response.json())
+    .then((json) => {
+        const convert_list = json.titles;
+        setList(convert_list);
+    })
+    .catch(err => {
+      console.log(err);
+    })
     return(
-        <Card sx={{maxWidth: 345}}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    alt="show image"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="subtitle1">
-                        Name of Show
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    )
+        <div className="list">
+            <div className="wrapper">
+                <div className="container">
+                    {list.map((show) => (
+                        <Card sx={{width: 225, height: 120, ml: 1}}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    image={show.jawSummary.backgroundImage.url}
+                                    height='110'
+                                    alt="show image"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1">
+                                        {show.jawSummary.title}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    ))}
+                </div>
+            </div>
+        </div>
+        // <Grid container spacing={3}>
+        //     <Grid container item spacing={2}>
+        //     {list.map((show) => (
+        //         <Grid item sx={{ml: 1}} key={show.jawSummary.id}>
+        //             <Card sx={{maxWidth: 345}}>
+        //                 <CardActionArea>
+        //                     <CardMedia
+        //                         component="img"
+        //                         height="140"
+        //                         image={show.jawSummary.backgroundImage.url}
+        //                         alt="show image"
+        //                     />
+        //                     <CardContent>
+        //                         <Typography gutterBottom variant="subtitle1">
+        //                             {show.jawSummary.title}
+        //                         </Typography>
+        //                     </CardContent>
+        //                 </CardActionArea>
+        //             </Card>
+        //         </Grid>
+        //     ))}
+        //     </Grid>
+        // </Grid>
+    );
 }
 
 export default ShowCard;
