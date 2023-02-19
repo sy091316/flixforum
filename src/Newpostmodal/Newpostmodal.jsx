a16a24aa8453490ad33bfaa2e5d32cf38cc0d5b5
Khin Yone <kyone@ucsc.edu>
2023-02-18 21:17:45 -0800

    changed some files, grouped some files, ui, and tried to add the search icon to search bar but did not work

--- src/Newpostmodal/Newpostmodal.jsx
+++ src/Newpostmodal/Newpostmodal.jsx
@@ -3,6 +3,7 @@ import Axios from 'axios'
 import { Link, useNavigate } from "react-router-dom";
 import '../Newpostmodal/Newpostmodal.css'
 //import close from './close.svg';
+import close from '../close.png';
 //Modal: https://medium.com/tinyso/how-to-create-a-modal-component-in-react-from-basic-to-advanced-a3357a2a716a
 
 export const Newpostmodal = (props) => {
@@ -30,7 +31,7 @@ export const Newpostmodal = (props) => {
             <div className="modal-content">
                 <div className="modal-header">
                     <h4 className="modal-title">New Post Forum</h4>
-                    {/*<img class="buttonclose" src={close} width="26" height="26" onClick={props.onClose}/> */}
+                    <img class="buttonclose" src={close} width="26" height="26" onClick={props.onClose}/> *
                 </div>
                 <div className="comment-title">
                 <textarea
--- src/Searchbar/searchbar.js
+++ src/Searchbar/searchbar.js
@@ -1,7 +1,7 @@
 import React , { useState } from 'react';
 import { useNavigate } from "react-router";
 import "./searchbar.css";
-//import image1, { ReactComponent as img1} from "./searchiconwhite.svg";
+import searchbar from "./searchbar.png"
 
 
 
@@ -24,18 +24,19 @@ export const Search = () => {
 
     return (
         <>
-       {/* <img src={image1} alt="img1" width="100%" height="100%" /> */}
-        <div className = "seach-bar-2"> 
-        <form className = "search-bar">
-        <input className= "search-bar"
+        <div className = "search-bar-icon">
+            {/* <div className = "icon"> */}
+                {/* <img className="searchicon" src={searchbar} />  */}
+            {/* </div> */}
+            <input className= "search-bar"
             size="50"
             type="text"
             placeholder="Enter a TV show"
             onChange={handleChange}
             onKeyUp={handleKeyPress}
             value={searchInput} />
-        </form>
-        </div> 
+           
+        </div>
         </>
 
     );
