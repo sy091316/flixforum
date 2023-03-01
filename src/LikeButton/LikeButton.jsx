764295e9c380c39a43f3e239286938a8bc924c4f
Julia Tober <jtober@ucsc.edu>
2023-03-01 15:35:13 -0800

    static like button working, but might want to add icons later

--- src/LikeButton/LikeButton.jsx
+++ src/LikeButton/LikeButton.jsx
@@ -2,23 +2,74 @@ import React, { useState } from 'react';
 
 
 const LikeButton = () => {
-  const [likes, setLikes] = useState(100);
-  const [isClicked, setIsClicked] = useState(false);
+  const [likeCount, setLikeCount] = useState(50);
+  const [dislikeCount, setDislikeCount] = useState(25);
 
-  const handleClick = () => {
-    if (isClicked) {
-      setLikes(likes - 1);
-    } else {
-      setLikes(likes + 1);
+  const [activeBtn, setActiveBtn] = useState("none");
+
+  const handleLikeClick = () => {
+    // need to do an axios post instead of using useState()
+    if (activeBtn === "none") {
+      setLikeCount(likeCount + 1); //"UPDATE Posts SET likes = likes + 1 WHERE id = ?"
+      setActiveBtn("like");
+      return;
     }
-    setIsClicked(!isClicked);
+
+    if (activeBtn === 'like'){
+      setLikeCount(likeCount - 1); //"UPDATE Posts SET likes = likes - 1 WHERE id = ?"
+      setActiveBtn("none");
+      return;
+    }
+
+    if (activeBtn === "dislike") {
+      setLikeCount(likeCount + 1); //"UPDATE Posts SET likes = likes + 1 WHERE id = ?"
+      setDislikeCount(dislikeCount - 1); //"UPDATE Posts SET dislikes = dislikes - 1 WHERE id = ?"
+      setActiveBtn("like");
+    }
   };
 
+  const handleDisikeClick = () => {
+    if (activeBtn === "none") {
+      setDislikeCount(dislikeCount + 1);
+      setActiveBtn("dislike");
+      return;
+    }
+    
+    if (activeBtn === 'dislike'){
+      setDislikeCount(dislikeCount - 1);
+      setActiveBtn("none");
+      return;
+    }
+
+    if (activeBtn === "like") {
+      setDislikeCount(dislikeCount + 1);
+      setLikeCount(likeCount - 1);
+      setActiveBtn("dislike");
+    }
+  };
+
+
   return (
-    <button className={ `like-button ${isClicked && 'liked'}` } onClick={ handleClick }>
-      <span className="likes-counter">{ `Like | ${likes}` }</span>
-    </button>
+    <div className="container">
+      <div className="btn-container">
+        <button
+          className={`btn ${activeBtn === 'like' ? 'like-active' : ''}`}
+          onClick={handleLikeClick}
+        >
+          <span className="material-symbols-rounded"></span>
+          Like {likeCount}
+        </button>
+
+        <button
+          className={`btn ${activeBtn === 'dislike' ? 'dislike-active' : ''}`}
+          onClick={handleDisikeClick}
+        >
+          <span className="material-symbols-rounded"></span>
+          Dislike {dislikeCount}
+        </button>
+      </div>
+    </div>
   );
-};
+}
 
 export default LikeButton;
