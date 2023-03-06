f3f95030b11f2a580931ce8ffe77448e3cfc69ae
Saki Yokokawa <sakiyokokawa@Sakis-MacBook-Air.local>
2023-03-05 16:58:48 -0800

    update newpost ui

--- src/Forum/Forum.jsx
+++ src/Forum/Forum.jsx
@@ -174,6 +174,23 @@ function Forum() {
                         </FormControl>
                     </Box>
                     <button onClick = {forum} className="button-go">GO</button>
+                    <div className="newpost-b">
+                        {
+                            (loginStatus || curr) ? 
+                            <div className="newpost">
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <button className="newpost-button" onClick={() => setShow(true)}><b>New Post</b></button>
+                                <Modal onClose = {() => setShow(false)} show={show}/>
+                            </div> : 
+                            // redirect to login if not logged in
+                            <div className="newpost">
+                                <br></br>
+                                <button type ="button" class = "newpost-button" onClick={() => navigate("/login")}><b>New Post</b></button>
+                            </div> 
+                        }
+                    </div>
                 </div>
             </div>
             {/* <div style={{color: '#FFFFFF'}}>{currEp && <div>curr episode: {currEp}</div>}</div> */}
@@ -189,21 +206,6 @@ function Forum() {
                     </Card>
                 ))}
             </div>
-            {
-                (loginStatus || curr) ? 
-                <div className="newpost">
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <button className="newpost-button" onClick={() => setShow(true)}><b>New Post</b></button>
-                    <Modal onClose = {() => setShow(false)} show={show}/>
-                </div> : 
-                // redirect to login if not logged in
-                <div className="newpost">
-                    <br></br>
-                    <button type ="button" class = "newpost-button" onClick={() => navigate("/login")}><b>New Post</b></button>
-                </div> 
-            }
         </div>
         </>
     )
