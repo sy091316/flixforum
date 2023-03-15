6be3b7ee6e0634e750fde3a921b089d42e25e01c
Gav1012 <gavin.poley@gmail.com>
2023-03-14 20:12:57 -0700

    added more comments to ShowCard

--- src/ShowCard/ShowCard.js
+++ src/ShowCard/ShowCard.js
@@ -18,25 +18,31 @@ function ShowCard() {
     const [list, setList] = useState([]);
     // handles how far you scroll through list of shows
     const [listPos, setListPos] = useState(0);
-
+    // holds the Netflix shows with > 1 season
     const [seasonList, setSeaList] = useState([]);
+    // handles the positioning of the shows when navigating for > 1 season
     const [seasonPos, setSeaPos] = useState(0);
+    // holds the Netflix shows with A in the title
     const [showAList, setShowAList] = useState([]);
+    // handles the positioning of the shows when navigating shows with A
     const [showAPos, setShowAPos] = useState(0);
     // handles updating the position of the shows cards
     const showRef = useRef();
     const seasonRef = useRef();
     const showARef = useRef();
+    // navigates to different pages of the application
     const navigate = useNavigate();
     // handles the arrow buttons for the first row of shows
     const handleClickAny = (direction) => {
         // holds the distance to travel between each show card when
         // button is clicked
         let distance = showRef.current.getBoundingClientRect().x - 50;
+        // if left move to shows going left
         if (direction === "left" && listPos > 0) {
             setListPos(listPos - 1);
             showRef.current.style.transform = `translateX(${275 + distance}px)`
         }
+        // if right move to shows going right
         if (direction === "right" && listPos < 5) {
             setListPos(listPos + 1);
             showRef.current.style.transform = `translateX(${-275 + distance}px)`
@@ -47,11 +53,13 @@ function ShowCard() {
         // holds the distance to travel between each show card when
         // button is clicked
         let distance = seasonRef.current.getBoundingClientRect().x - 50;
+        // if left move to shows going left
         if (direction === "left" && seasonPos > 0) {
             setSeaPos(seasonPos - 1);
             seasonRef.current.style.transform = `translateX(${275 + distance}px)`
         }
-        if (direction === "right" && seasonPos < 2) {
+        // if right move to shows going right
+        if (direction === "right" && seasonPos < 5) {
             setSeaPos(seasonPos + 1);
             seasonRef.current.style.transform = `translateX(${-275 + distance}px)`
         }
@@ -61,10 +69,12 @@ function ShowCard() {
         // holds the distance to travel between each show card when
         // button is clicked
         let distance = showARef.current.getBoundingClientRect().x - 50;
+        // if left move to shows going left
         if (direction === "left" && showAPos > 0) {
             setShowAPos(showAPos - 1);
             showARef.current.style.transform = `translateX(${275 + distance}px)`
         }
+        // if right move to shows going right
         if (direction === "right" && showAPos < 5) {
             setShowAPos(showAPos + 1);
             showARef.current.style.transform = `translateX(${-275 + distance}px)`
@@ -83,9 +93,9 @@ function ShowCard() {
             }
         })
         .then(response => response.json())
-        // sets the
         .then((json) => {
             const convert_list = json.titles;
+            // sets list of Netflix shows grabbed from API
             setList(convert_list);
             setSeaList(convert_list);
         })
@@ -103,9 +113,9 @@ function ShowCard() {
             }
         })
         .then(response => response.json())
-        // sets the
         .then((json) => {
             const convert_list = json.titles;
+            // sets the list of Netflix shows with A in title from API
             setShowAList(convert_list);
         })
         .catch(err => {
@@ -115,8 +125,6 @@ function ShowCard() {
 
     return(
         <div className="list">
-            <br></br>
-            <br></br>
             <div className="recommend"><b>Popular on Netflix</b></div>
             <br></br>
             <div className="wrapper">
@@ -124,11 +132,13 @@ function ShowCard() {
                 <div className="slider arrowboxleft" onClick={()=>handleClickAny("left")}>
                     <ArrowBackIosOutlined className="slider leftarrow"/>
                 </div>
+                {/* Maps out all the shows from the Netflix API into row of cards*/}
                 <div className="container" ref={showRef} >
                     {list.map((show) => (
                         show.summary.type === 'show' ?
                         <div className="list-cards" key={show.summary.id}>
                             <Card sx={{width: 275, height: 200, ml: 1, backgroundColor: "#43465e"}}>
+                                {/* sets the data that will passed to forum page*/}
                                 <CardActionArea key={show.summary.id} onClick={ () => {
                                             localStorage.setItem('showID', JSON.stringify(show.summary.id));
                                             localStorage.setItem('title', JSON.stringify(show.jawSummary.title));
@@ -153,6 +163,7 @@ function ShowCard() {
                                     </CardContent>
                                 </CardActionArea>
                             </Card>
+                        {/* if nothing is retrieved from API, show nothing*/}
                         </div>
                         : null
                     ))}
@@ -163,7 +174,6 @@ function ShowCard() {
                 </div>
             </div>
             <br></br>
-            <br></br>
             <div className="recommend"><b>Shows With More than One Season</b></div>
             <br></br>
             <div className="wrapper">
@@ -171,11 +181,13 @@ function ShowCard() {
                 <div className="slider arrowboxleft" onClick={()=>handleClickMSeason("left")}>
                     <ArrowBackIosOutlined className="slider leftarrow"/>
                 </div>
+                {/* Maps out all the shows from the Netflix API with > 1 season into row of cards*/}
                 <div className="container" ref={seasonRef}>
                     {seasonList.map((show) => (
                         show.summary.type === 'show' && show.jawSummary.seasonCount > 1 ?
                         <div className="list-cards" key={show.summary.id}>
                             <Card sx={{width: 275, height: 200, ml: 1, backgroundColor: "#43465e"}}>
+                                {/* sets the data that will passed to forum page*/}
                                 <CardActionArea onClick={ () => {
                                             localStorage.setItem('showID', JSON.stringify(show.summary.id));
                                             localStorage.setItem('title', JSON.stringify(show.jawSummary.title));
@@ -200,6 +212,7 @@ function ShowCard() {
                                     </CardContent>
                                 </CardActionArea>
                             </Card>
+                        {/* if nothing is retrieved from API, show nothing*/}
                         </div>
                         : null
                     ))}
@@ -210,7 +223,6 @@ function ShowCard() {
                 </div>
             </div>
             <br></br>
-            <br></br>
             <div className="recommend"><b>Shows A in the Name</b></div>
             <br></br>
             <div className="wrapper">
@@ -218,11 +230,13 @@ function ShowCard() {
                 <div className="slider arrowboxleft" onClick={()=>handleClickAShow("left")}>
                     <ArrowBackIosOutlined className="slider leftarrow"/>
                 </div>
+                {/* Maps out all the shows from the Netflix API with A in the title into row of cards*/}
                 <div className="container" ref={showARef} >
                     {showAList.map((show) => (
                         show.summary.type === 'show' ?
                         <div className="list-cards" key={show.summary.id}>
                             <Card sx={{width: 275, height: 200, ml: 1, backgroundColor: "#43465e"}}>
+                                {/* sets the data that will passed to forum page*/}
                                 <CardActionArea onClick={ () => {
                                             localStorage.setItem('showID', JSON.stringify(show.summary.id));
                                             localStorage.setItem('title', JSON.stringify(show.jawSummary.title));
@@ -247,6 +261,7 @@ function ShowCard() {
                                     </CardContent>
                                 </CardActionArea>
                             </Card>
+                        {/* if nothing is retrieved from API, show nothing*/}
                         </div>
                         : null
                     ))}
@@ -256,6 +271,7 @@ function ShowCard() {
                     <ArrowForwardIosOutlined className="slider rightarrow"/>
                 </div>
             </div>
+            <br></br>
         </div>
     );
 }
